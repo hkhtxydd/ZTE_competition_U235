daa1884b4d9ddbde6005196c79fb7f673eab97a4
hkhtxydd <13096353290@163.com>
2019-04-01 18:03:58 +0800

    Update

--- ZTE_competition/main.cpp
+++ ZTE_competition/main.cpp
@@ -1,91 +1,10 @@
 ﻿#include "main.h"
 
-
-using namespace std;
-
-
-
-
-
-
-void str2int_Port(
-	const string& s,
-	unsigned int & Port_begin,
-	unsigned int & Port_end) {
-	vector<string> tmp_1;
-	SplitString(s, tmp_1, ":");
-	Port_begin = (unsigned int)stoi(tmp_1[0]);
-	Port_end = (unsigned int)stoi(tmp_1[1]);
-}
-
-void str2int_protocol(
-	const string& s,
-	unsigned int & protocol,
-	unsigned int & protocol_mask) {
-	vector<string> tmp_1;
-	SplitString(s, tmp_1, "/");
-	protocol = (unsigned int)stoi(tmp_1[0]);
-	protocol_mask = (unsigned int)stoi(tmp_1[1]);
-}
-
-
-void rule2dataAndMask(st_rule oneRule, string& s){
-	int i, j, k;
-	for (i = oneRule.destination_port_begin; i <= oneRule.destination_port_end; i++) {
-		for (j = oneRule.source_port_begin; j <= oneRule.source_port_end; j++)
-		{
-			cout << "data:";
-			cout << "0x";
-			cout << hex << oneRule.destination_ip_0 * 256 * 256 * 256+ oneRule.destination_ip_1 * 256 * 256+ oneRule.destination_ip_2 * 256+ oneRule.destination_ip_3;
-			cout << " ";
-
-			cout << "0x";
-			cout << hex << oneRule.source_ip_0 * 256 * 256 * 256 + oneRule.source_ip_1 * 256 * 256 + oneRule.source_ip_2 * 256 + oneRule.source_ip_3;
-			cout << " ";
-
-			cout << "0x";
-			cout << hex << i;
-			cout << " ";
-
-			cout << "0x";
-			cout << hex << j;
-			cout << " ";
-
-			cout <<dec<< oneRule.protocol_type;
-			cout << endl;
-
-
-			cout << "mask:";
-			cout << "0x";
-			int destination_MASK = INT_MAX;
-			destination_MASK = destination_MASK << (32 - oneRule.destination_ip_mask);
-			cout << hex << destination_MASK;
-			cout << " ";
-
-			cout << "0x";
-			int source_MASK = INT_MAX;
-			source_MASK = source_MASK << (32 - oneRule.destination_ip_mask);
-			cout << hex << source_MASK;
-			cout << " ";
-
-			cout << "0xffff 0xffff 0xff";
-			cout << endl;
-			
-			
-
-		}
-	}
-
-
-}
-
 int main()
 {
 	int i, j, k;
-	ifstream inFile;
+	
 	inFile.open( inputDataFile );
-
-	ofstream outFile;
 	outFile.open(outputDataFile);
 
 	vector<string> rule_str;
@@ -110,59 +29,96 @@ int main()
 		message_str.push_back(one_message);
 	}
 
-	vector<st_rule> rules;
+	vector<st_rule * > rules;
 
 	for (i = 0; i < rule_number; i++) {
 
-		st_rule oneRule;
+		UINT32 destination_ip;
+		UINT32 destination_ip_mask;
+		UINT32 source_ip;
+		UINT32 source_ip_mask;
+		UINT16 destination_port;
+		UINT16 source_port;
+		UINT8 protocol;
+		UINT32 type;
 
 		vector<string> oneRule_split;
 		SplitString(rule_str[i], oneRule_split, " ");
 		{
 			str2int_IpAndMask(
 				oneRule_split[0],
-				oneRule.destination_ip_0,
-				oneRule.destination_ip_1,
-				oneRule.destination_ip_2,
-				oneRule.destination_ip_3,
-				oneRule.destination_ip_mask
+				destination_ip,
+				destination_ip_mask
 				);
 			str2int_IpAndMask(
 				oneRule_split[1],
-				oneRule.source_ip_0,
-				oneRule.source_ip_1,
-				oneRule.source_ip_2,
-				oneRule.source_ip_3,
-				oneRule.source_ip_mask
-				);
-			str2int_Port(
-				oneRule_split[2],
-				oneRule.destination_port_begin,
-				oneRule.destination_port_end
+				source_ip,
+				source_ip_mask
 			);
-			str2int_Port(
-				oneRule_split[3],
-				oneRule.source_port_begin,
-				oneRule.source_port_end
-			);
-			str2int_protocol(
-				oneRule_split[4],
-				oneRule.protocol,
-				oneRule.protocol_mask
-			);
-			oneRule.protocol_type = stoi(oneRule_split[5]);
+
+			str2int_protocol(oneRule_split[4], protocol);
+
+			type = (UINT32)stoi(oneRule_split[5]);
+
+			vector<string> destination_port_split;
+			SplitString(oneRule_split[2], destination_port_split, ":");
+			int destination_port_begin = stoi(destination_port_split[0]);
+			int destination_port_end = stoi(destination_port_split[1]);
+			for (j = destination_port_begin; j <= destination_port_end; j++) {
+				vector<string> source_port_split;
+				SplitString(oneRule_split[3], source_port_split, ":");
+				int source_port_begin = stoi(source_port_split[0]);
+				int source_port_end = stoi(source_port_split[1]);
+				for (k = source_port_begin; k <= source_port_end; k++) {
+					st_rule * oneRule=new st_rule;
+					oneRule->destination_ip = destination_ip;
+					oneRule->destination_ip_mask = destination_ip_mask;
+					oneRule->destination_port = j;
+
+					oneRule->source_ip = source_ip;
+					oneRule->source_ip_mask = source_ip_mask;
+					oneRule->source_port = k;
+
+					oneRule->protocol = protocol;
+
+					oneRule->type = type;
+					rules.push_back(oneRule);
+
+					rule2dataAndMask(*(rules.end() - 1));
+				}
+			}
 		}
-		rules.push_back(oneRule);
+	}
 
-		string sstr;
-		rule2dataAndMask(oneRule, sstr);
-		system("pause");
 
+	vector<st_message * > messages;
+
+	vector<string> oneMessage_split;
+	for (i = 0; i < message_number; i++) {
+		SplitString(message_str[i], oneMessage_split, " ");
+		st_message * oneMessage = new st_message;
+		str2ip(oneMessage_split[0], oneMessage->destination_ip);
+		str2ip(oneMessage_split[1], oneMessage->source_ip);
+		oneMessage->destination_port = stoi(oneMessage_split[2]);
+		oneMessage->source_port = stoi(oneMessage_split[3]);
+		oneMessage->protocol = stoi(oneMessage_split[4]);
+		messages.push_back(oneMessage);
+	}
+
+	//匹配报文
+
+	for (i = 0; i < message_number; i++){
+		st_message * oneMessage = messages[i];
+		for (j = 0; j < rule_number; j++) {
+			st_rule * oneRule = rules[j];
+			st_message * tmp = (*oneMessage) & (*((st_message *)(oneRule)));
+		}
 
 	}
 
 
-
+	inFile.close();
+	outFile.close();
 	system("pause");
 	return 0;
 }
--- ZTE_competition/main.h
+++ ZTE_competition/main.h
@@ -10,13 +10,19 @@
 
 using namespace std;
 
-#define inputDataFile ("C:/Users/LJY/source/repos/ZTE_competition/in.txt")
+#define inputDataFile ("../testData/in.txt")
 
-#define outputDataFile ("C:/Users/LJY/source/repos/ZTE_competition/out.txt")
+#define outputDataFile ("../testData/myout.txt")
 
 typedef unsigned int UINT32;
 typedef unsigned short UINT16;
 typedef unsigned char UINT8;
+
+ifstream inFile;
+ofstream outFile;
+
+#define myStreamOut outFile
+
 
 void SplitString(const string& s, vector<string>& v, const string& c)
 {
@@ -35,18 +41,26 @@ void SplitString(const string& s, vector<string>& v, const string& c)
 		v.push_back(s.substr(pos1));
 }
 
+struct st_compareBits
+{
+	UINT32 a;
+	UINT32 b;
+	UINT32 c;
+	UINT8 d;
+};
+
 struct st_rule
 {
 	UINT32 destination_ip;
-	UINT32 destination_ip_mask;
-
 	UINT32 source_ip;
-	UINT32 source_ip_mask;
-	
 	UINT16 destination_port;
 	UINT16 source_port;
-
 	UINT8 protocol;
+
+	UINT32 destination_ip_mask;
+	UINT32 source_ip_mask;
+	UINT32 type;
+
 };
 
 void str2int_IpAndMask(
@@ -57,9 +71,55 @@ void str2int_IpAndMask(
 	SplitString(s, tmp_1, "/");
 	vector<string> tmp_2;
 	SplitString(tmp_1[0], tmp_2, ".");
-	ip = (UINT32)stoi(tmp_2[0])<<24 + (UINT32)stoi(tmp_2[1]) << 16 + (UINT32)stoi(tmp_2[2]) << 8 + (UINT32)stoi(tmp_2[3]) << 0;
+	ip = (((UINT32)(stoi(tmp_2[0]))) << 24) + (((UINT32)(stoi(tmp_2[1]))) << 16) + (((UINT32)(stoi(tmp_2[2]))) << 8) + (UINT32)(stoi(tmp_2[3]));
 
 	int source_MASK = INT_MAX;
 	ip_mask = source_MASK << (32 - stoi(tmp_1[1]));
+
+}
+
+void str2int_protocol(
+	const string& s,
+	UINT8 & protocol) {
+	vector<string> tmp_1;
+	SplitString(s, tmp_1, "/");
+
+	vector<string> tmp_2;
+	SplitString(tmp_1[0], tmp_2, "x");
+	protocol = (UINT8)stoi(tmp_2[1], nullptr,16);
+}
+
+void rule2dataAndMask(st_rule * oneRule) {
+
+	myStreamOut << "data:";
+	myStreamOut << "0x" << hex << oneRule->destination_ip << " ";
+	myStreamOut << "0x" << hex << oneRule->source_ip << " ";
+	myStreamOut << "0x" << hex << oneRule->destination_port << " ";
+	myStreamOut << "0x" << hex << oneRule->source_port << " ";
+	myStreamOut << "0x" << hex << (UINT32)(oneRule->protocol) << " ";
+	myStreamOut << dec << oneRule->type << endl;
+
+	myStreamOut << "mask:";
+	myStreamOut << "0x" << hex << oneRule->destination_ip_mask << " ";
+	myStreamOut << "0x" << hex << oneRule->source_ip_mask << " ";
+	myStreamOut << "0xffff 0xffff 0xff" << endl;
+}
+
+struct st_message
+{
+	UINT32 destination_ip;
+	UINT32 source_ip;
+	UINT16 destination_port;
+	UINT16 source_port;
+	UINT8 protocol;
+};
+
+void str2ip(
+	const string& s,
+	UINT32 & ip) {
+
+	vector<string> tmp_1;
+	SplitString(s, tmp_1, ".");
+	ip = (((UINT32)(stoi(tmp_1[0]))) << 24) + (((UINT32)(stoi(tmp_1[1]))) << 16) + (((UINT32)(stoi(tmp_1[2]))) << 8) + (UINT32)(stoi(tmp_1[3]));
 
 }
