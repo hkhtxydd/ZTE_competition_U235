2f39dd33b7be55adedb2880adb64c8a2afad1416
hkhtxydd <13096353290@163.com>
2019-04-02 10:41:34 +0800

    update

--- ZTE_competition/main.cpp
+++ ZTE_competition/main.cpp
@@ -1,44 +1,174 @@
 ﻿#include "main.h"
 
+
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <istream>
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+#define inputDataFile ("../testData/int_1.txt")
+
+#define outputDataFile ("../testData/myout.txt")
+
+typedef unsigned int UINT32;
+typedef unsigned short UINT16;
+typedef unsigned char UINT8;
+
+ifstream inFile;
+ofstream outFile;
+
+#define myStreamIn inFile
+#define myStreamOut outFile
+
+void SplitString(const string& s, vector<string>& v, const string& c)
+{
+	v.clear();
+	string::size_type pos1, pos2;
+	pos2 = s.find(c);
+	pos1 = 0;
+	while (string::npos != pos2)
+	{
+		v.push_back(s.substr(pos1, pos2 - pos1));
+
+		pos1 = pos2 + c.size();
+		pos2 = s.find(c, pos1);
+	}
+	if (pos1 != s.length())
+		v.push_back(s.substr(pos1));
+}
+
+struct st_rule
+{
+	UINT32 destination_ip;
+	UINT32 source_ip;
+
+	UINT16 destination_port;
+	UINT16 source_port;
+	UINT16 destination_port_mask;
+	UINT16 source_port_mask;
+
+	UINT8 protocol;
+
+	UINT32 destination_ip_mask;
+	UINT32 source_ip_mask;
+	UINT32 type;
+
+};
+
+void str2int_IpAndMask(
+	const string& s,
+	UINT32 & ip,
+	UINT32 & ip_mask) {
+	vector<string> tmp_1;
+	SplitString(s, tmp_1, "/");
+	vector<string> tmp_2;
+	SplitString(tmp_1[0], tmp_2, ".");
+	ip = (((UINT32)(stoi(tmp_2[0]))) << 24) + (((UINT32)(stoi(tmp_2[1]))) << 16) + (((UINT32)(stoi(tmp_2[2]))) << 8) + (UINT32)(stoi(tmp_2[3]));
+
+	int source_MASK = INT_MAX;
+	ip_mask = source_MASK << (32 - stoi(tmp_1[1]));
+
+}
+
+void str2int_protocol(
+	const string& s,
+	UINT8 & protocol) {
+	vector<string> tmp_1;
+	SplitString(s, tmp_1, "/");
+
+	vector<string> tmp_2;
+	SplitString(tmp_1[0], tmp_2, "x");
+	protocol = (UINT8)stoi(tmp_2[1], nullptr, 16);
+}
+
+void rule2dataAndMask(st_rule * oneRule) {
+	int i, j;
+
+			myStreamOut << "data:";
+			myStreamOut << "0x" << hex << oneRule->destination_ip << " ";
+			myStreamOut << "0x" << hex << oneRule->source_ip << " ";
+			myStreamOut << "0x" << hex << oneRule->destination_port<< " ";
+			myStreamOut << "0x" << hex << oneRule->source_port << " ";
+			myStreamOut << "0x" << hex << (UINT32)(oneRule->protocol) << " ";
+			myStreamOut << dec << oneRule->type << endl;
+
+			myStreamOut << "mask:";
+			myStreamOut << "0x" << hex << oneRule->destination_ip_mask << " ";
+			myStreamOut << "0x" << hex << oneRule->source_ip_mask << " ";
+			myStreamOut << "0x" << hex << oneRule->destination_port_mask << " ";
+			myStreamOut << "0x" << hex << oneRule->source_port_mask << " ";
+			myStreamOut << endl;
+
+}
+
+struct st_message
+{
+	UINT32 destination_ip;
+	UINT32 source_ip;
+	UINT16 destination_port;
+	UINT16 source_port;
+	UINT8 protocol;
+};
+
+void str2ip(
+	const string& s,
+	UINT32 & ip) {
+
+	vector<string> tmp_1;
+	SplitString(s, tmp_1, ".");
+	ip = (((UINT32)(stoi(tmp_1[0]))) << 24) + (((UINT32)(stoi(tmp_1[1]))) << 16) + (((UINT32)(stoi(tmp_1[2]))) << 8) + (UINT32)(stoi(tmp_1[3]));
+
+}
+
 int main()
 {
 	int i, j, k;
-	
+
 	inFile.open( inputDataFile );
 	outFile.open(outputDataFile);
 
+
+
 	vector<string> rule_str;
 	string rule_number_str;
-	getline(inFile, rule_number_str);
+	getline(myStreamIn, rule_number_str);
 	int rule_number = stoi(rule_number_str);
 
 	for (auto i = 0; i < rule_number; i++) {
 		string one_rule;
-		getline(inFile, one_rule);
+		getline(myStreamIn, one_rule);
 		rule_str.push_back(one_rule);
 	}
 
 	string message_number_str;
 	vector<string> message_str;
-	getline(inFile, message_number_str);
+	getline(myStreamIn, message_number_str);
 	int message_number = stoi(message_number_str);
 
 	for (i = 0; i < message_number; i++) {
 		string one_message;
-		getline(inFile, one_message);
+		getline(myStreamIn, one_message);
 		message_str.push_back(one_message);
 	}
 
 	vector<st_rule * > rules;
 
 	for (i = 0; i < rule_number; i++) {
-
+		cout << "RULE:\t" << i << endl;
 		UINT32 destination_ip;
 		UINT32 destination_ip_mask;
 		UINT32 source_ip;
 		UINT32 source_ip_mask;
-		UINT16 destination_port;
-		UINT16 source_port;
+		UINT16 destination_port_begin;
+		UINT16 destination_port_end;
+		UINT16 source_port_begin;
+		UINT16 source_port_end;
 		UINT8 protocol;
 		UINT32 type;
 
@@ -49,7 +179,7 @@ int main()
 				oneRule_split[0],
 				destination_ip,
 				destination_ip_mask
-				);
+			);
 			str2int_IpAndMask(
 				oneRule_split[1],
 				source_ip,
@@ -62,40 +192,68 @@ int main()
 
 			vector<string> destination_port_split;
 			SplitString(oneRule_split[2], destination_port_split, ":");
-			int destination_port_begin = stoi(destination_port_split[0]);
-			int destination_port_end = stoi(destination_port_split[1]);
-			for (j = destination_port_begin; j <= destination_port_end; j++) {
-				vector<string> source_port_split;
-				SplitString(oneRule_split[3], source_port_split, ":");
-				int source_port_begin = stoi(source_port_split[0]);
-				int source_port_end = stoi(source_port_split[1]);
-				for (k = source_port_begin; k <= source_port_end; k++) {
-					st_rule * oneRule=new st_rule;
+			destination_port_begin = stoi(destination_port_split[0]);
+			destination_port_end = stoi(destination_port_split[1]);
+
+			vector<string> source_port_split;
+			SplitString(oneRule_split[3], source_port_split, ":");
+			source_port_begin = stoi(source_port_split[0]);
+			source_port_end = stoi(source_port_split[1]);
+
+			vector<st_port_mask*>  destination_port_mask_vector;
+			vector<st_port_mask*>  source_port_mask_vector;
+
+			if (destination_port_begin == destination_port_end){
+				st_port_mask* port_mask = new st_port_mask;
+				port_mask->mask = 0xffff;
+				port_mask->port = destination_port_begin;
+				destination_port_mask_vector.push_back(port_mask);
+			}
+			else{
+				m_main(destination_port_begin, destination_port_end, destination_port_mask_vector);
+			}
+			
+			if (source_port_begin == source_port_end) {
+				st_port_mask* port_mask = new st_port_mask;
+				port_mask->mask = 0xffff;
+				port_mask->port = source_port_begin;
+				source_port_mask_vector.push_back(port_mask);
+			}
+			else {
+				m_main(source_port_begin, source_port_end, source_port_mask_vector);
+			}
+
+			for (j = 0; j < destination_port_mask_vector.size(); j++) {
+				for (k = 0; k < source_port_mask_vector.size(); k++) {
+
+					st_rule * oneRule = new st_rule;
 					oneRule->destination_ip = destination_ip;
 					oneRule->destination_ip_mask = destination_ip_mask;
-					oneRule->destination_port = j;
-
 					oneRule->source_ip = source_ip;
 					oneRule->source_ip_mask = source_ip_mask;
-					oneRule->source_port = k;
+
+					oneRule->destination_port = destination_port_mask_vector[j]->port;
+					oneRule->destination_port_mask = destination_port_mask_vector[j]->mask;
+					oneRule->source_port = source_port_mask_vector[k]->port;
+					oneRule->source_port_mask = source_port_mask_vector[k]->mask;
 
 					oneRule->protocol = protocol;
 
 					oneRule->type = type;
 					rules.push_back(oneRule);
-
-					rule2dataAndMask(*(rules.end() - 1));
 				}
 			}
 		}
 	}
-
+	cout << "DONE" << endl;
 
 	vector<st_message * > messages;
 
 	vector<string> oneMessage_split;
 	for (i = 0; i < message_number; i++) {
-		SplitString(message_str[i], oneMessage_split, " ");
+
+		auto tmp = message_str[i];
+		SplitString(tmp, oneMessage_split, " ");
 		st_message * oneMessage = new st_message;
 		str2ip(oneMessage_split[0], oneMessage->destination_ip);
 		str2ip(oneMessage_split[1], oneMessage->source_ip);
@@ -105,13 +263,34 @@ int main()
 		messages.push_back(oneMessage);
 	}
 
+	//输出规则
+	myStreamOut << "32 32 16 16 8 8" << endl;
+	for (i = 0; i < rules.size(); i++) {
+		rule2dataAndMask(rules[i]);
+	}
 	//匹配报文
 
-	for (i = 0; i < message_number; i++){
+	for (i = 0; i < message_number; i++) {
 		st_message * oneMessage = messages[i];
 		for (j = 0; j < rule_number; j++) {
 			st_rule * oneRule = rules[j];
-			st_message * tmp = (*oneMessage) & (*((st_message *)(oneRule)));
+			st_message result;
+
+			result.destination_ip = ((oneMessage->destination_ip) ^ (oneRule->destination_ip))&(oneRule->destination_ip_mask);
+			result.source_ip = ((oneMessage->source_ip) ^ (oneRule->source_ip))&(oneRule->source_ip_mask);
+
+			result.destination_port= ((oneMessage->destination_port) ^ (oneRule->destination_port))&(oneRule->destination_port_mask);
+			result.source_port = ((oneMessage->source_port) ^ (oneRule->source_port))&(oneRule->source_port_mask);
+
+			result.protocol = oneMessage->protocol ^ oneRule->protocol;
+
+			if(result.destination_ip+ result.source_ip+ result.destination_port+ result.source_port + result.protocol==0) {
+				j = rule_number + 1;
+				myStreamOut << dec << oneRule->type << endl;
+			}
+		}
+		if (j == rule_number) {
+			myStreamOut << 0 << endl;
 		}
 
 	}
@@ -119,6 +298,6 @@ int main()
 
 	inFile.close();
 	outFile.close();
-	system("pause");
+	//system("pause");
 	return 0;
 }
--- ZTE_competition/main.h
+++ ZTE_competition/main.h
@@ -1,125 +1,267 @@
 #pragma once
-#include <iostream>
-#include <fstream>
-#include <ostream>
-#include <istream>
 
+#include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
 
+struct st_port_mask
+{
+	unsigned short port;
+	unsigned short mask;
+};
+
 using namespace std;
-
-#define inputDataFile ("../testData/in.txt")
-
-#define outputDataFile ("../testData/myout.txt")
-
-typedef unsigned int UINT32;
-typedef unsigned short UINT16;
-typedef unsigned char UINT8;
-
-ifstream inFile;
-ofstream outFile;
-
-#define myStreamOut outFile
-
-
-void SplitString(const string& s, vector<string>& v, const string& c)
-{
-
-	string::size_type pos1, pos2;
-	pos2 = s.find(c);
-	pos1 = 0;
-	while (string::npos != pos2)
-	{
-		v.push_back(s.substr(pos1, pos2 - pos1));
-
-		pos1 = pos2 + c.size();
-		pos2 = s.find(c, pos1);
+vector<string>input;
+int maxi, store[100000];
+//convert decimal to binary
+string dec_bin(int num, int var) {
+	string a;
+	int al = 0;
+	for (; num > 0; num /= 2) {
+		char temp = num % 2 + '0';
+		a.push_back(temp);
+		al++;
 	}
-	if (pos1 != s.length())
-		v.push_back(s.substr(pos1));
+	for (int j = 0; j < var - al; j++) {
+		a.push_back('0');
+	}
+	reverse(a.begin(), a.end());
+	return a;
+}
+//input vector number of bits= number of variables
+void in(int n, int var) {
+	int num;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		store[i] = num;
+		input.push_back(dec_bin(num, var));
+	}
 }
 
-struct st_compareBits
-{
-	UINT32 a;
-	UINT32 b;
-	UINT32 c;
-	UINT8 d;
-};
-
-struct st_rule
-{
-	UINT32 destination_ip;
-	UINT32 source_ip;
-	UINT16 destination_port;
-	UINT16 source_port;
-	UINT8 protocol;
-
-	UINT32 destination_ip_mask;
-	UINT32 source_ip_mask;
-	UINT32 type;
-
-};
-
-void str2int_IpAndMask(
-	const string& s,
-	UINT32 & ip,
-	UINT32 & ip_mask) {
-	vector<string> tmp_1;
-	SplitString(s, tmp_1, "/");
-	vector<string> tmp_2;
-	SplitString(tmp_1[0], tmp_2, ".");
-	ip = (((UINT32)(stoi(tmp_2[0]))) << 24) + (((UINT32)(stoi(tmp_2[1]))) << 16) + (((UINT32)(stoi(tmp_2[2]))) << 8) + (UINT32)(stoi(tmp_2[3]));
-
-	int source_MASK = INT_MAX;
-	ip_mask = source_MASK << (32 - stoi(tmp_1[1]));
-
+//input vector number of bits= number of variables
+void m_in(int a, int b, int var) {
+	int num;
+	for (int i = 0; i < b - a + 1; i++) {
+		//cin >> num;
+		num = a + i;
+		store[i] = num;
+		input.push_back(dec_bin(num, var));
+	}
 }
 
-void str2int_protocol(
-	const string& s,
-	UINT8 & protocol) {
-	vector<string> tmp_1;
-	SplitString(s, tmp_1, "/");
-
-	vector<string> tmp_2;
-	SplitString(tmp_1[0], tmp_2, "x");
-	protocol = (UINT8)stoi(tmp_2[1], nullptr,16);
+//check if binary number differ by only one bit
+int one_bit(string a, string b) {
+	int flag = 0;
+	for (int i = 0; i < a.length(); i++) {
+		if (a[i] != b[i]) { flag++; }
+	}
+	if (flag == 1) { return 1; }
+	return 0;
+}
+//if bit differ by only one bit we assign that bit -
+string elim_if_comp(string a, string b) {
+	string temp;
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] != b[i]) { temp = temp + "-"; }
+		else { temp = temp + a[i]; }
+	}
+	return temp;
+}
+//check if temp pre-exist in vector 
+int compar(vector<string> a, string b) {
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i].compare(b) == 0) { return 0; }
+	}
+	return 1;
+}
+//comparing two numbers with same bit_one and changing there terms 
+vector<string> step2(vector<string>a) {
+	vector<string>retu_vec;
+	retu_vec.clear();
+	int *flag;
+	flag = new int[a.size()];
+	memset(flag, 0, sizeof(flag));
+	for (int i = 0; i < a.size(); i++) {
+		for (int j = i; j < a.size(); j++) {
+			if (one_bit(a[i], a[j])) {
+				flag[i] = flag[j] = 1;
+				if (compar(retu_vec, elim_if_comp(a[i], a[j]))) {
+					retu_vec.push_back(elim_if_comp(a[i], a[j]));
+				}
+			}
+		}
+	}
+	for (int i = 0; i < a.size(); i++) {
+		if (flag[i] != 1 && compar(retu_vec, a[i])) {
+			retu_vec.push_back(a[i]);
+		}
+	}
+	//free(flag);
+	return retu_vec;
+}
+int compare_vect(vector<string>a, vector<string>b) {
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] != b[i]) { return 1; }
+	}
+	return 0;
+}
+int coun(string a) {
+	int cou = 0;
+	for (int i = 0; a[i]; i++) {
+		cou += a[i] == '-' ? 1 : 0;
+	}
+	return cou;
+}
+vector<string> sortin(vector<string>in) {
+	vector<pair<int, string> >gullu;
+	for (int i = 0; i < input.size(); i++) {
+		gullu.push_back(make_pair(coun(input[i]), input[i]));
+	}
+	sort(gullu.begin(), gullu.end());
+	vector<string>hofaba;
+	for (int i = 0; i < gullu.size(); i++) {
+		hofaba.push_back(gullu[i].second);
+	}
+	return hofaba;
 }
 
-void rule2dataAndMask(st_rule * oneRule) {
-
-	myStreamOut << "data:";
-	myStreamOut << "0x" << hex << oneRule->destination_ip << " ";
-	myStreamOut << "0x" << hex << oneRule->source_ip << " ";
-	myStreamOut << "0x" << hex << oneRule->destination_port << " ";
-	myStreamOut << "0x" << hex << oneRule->source_port << " ";
-	myStreamOut << "0x" << hex << (UINT32)(oneRule->protocol) << " ";
-	myStreamOut << dec << oneRule->type << endl;
-
-	myStreamOut << "mask:";
-	myStreamOut << "0x" << hex << oneRule->destination_ip_mask << " ";
-	myStreamOut << "0x" << hex << oneRule->source_ip_mask << " ";
-	myStreamOut << "0xffff 0xffff 0xff" << endl;
+int dash_compare(string a, string b) {
+	for (int i = 0; i < a.length(); i++) {
+		if (a[i] != b[i] && a[i] != '-') { return 0; }
+	}
+	return 1;
 }
 
-struct st_message
-{
-	UINT32 destination_ip;
-	UINT32 source_ip;
-	UINT16 destination_port;
-	UINT16 source_port;
-	UINT8 protocol;
-};
 
-void str2ip(
-	const string& s,
-	UINT32 & ip) {
+int m_main(int m_begin, int m_end, vector<st_port_mask*> & port_mask_vector) {
+	//input
+	int n, var;
 
-	vector<string> tmp_1;
-	SplitString(s, tmp_1, ".");
-	ip = (((UINT32)(stoi(tmp_1[0]))) << 24) + (((UINT32)(stoi(tmp_1[1]))) << 16) + (((UINT32)(stoi(tmp_1[2]))) << 8) + (UINT32)(stoi(tmp_1[3]));
+	var = 16;
 
+	n = m_end - m_begin + 1;
+	string all_dash(var, '-');
+
+	m_in(m_begin, m_end, var);
+
+	//determination of prime implicant
+	sort(store, store + n);
+	while (compare_vect(input, step2(input))) {
+		input = step2(input);
+	}
+	input = sortin(input);
+	int **gra = new int*[input.size()];
+	for (int i = 0; i < input.size(); i++) {
+		gra[i] = new int[n];
+	}
+	for (int i = 0; i < input.size(); i++) {
+		for (int j = 0; j < n; j++) {
+			gra[i][j] = 0;
+			if (dash_compare(input[i], dec_bin(store[j], var))) { gra[i][j] = 1; }
+		}
+	}
+	//selection of prime implicant
+	vector<string>output;
+	output.clear();
+	int check_val = 1;
+	while (1) {
+		if (check_val == 0) { break; }
+		check_val = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < input.size(); j++) {
+				if (gra[j][i] != 0) { check_val = 1; break; }
+			}
+			if (check_val == 1) { break; }
+		}
+
+		vector<int>count_1;
+		//count number of ones and push it to count_1
+		for (int i = 0; i < n; i++) {
+			int abc = 0;
+			for (int j = 0; j < input.size(); j++) {
+				if (gra[j][i] == 1) { abc++; }
+			}
+			count_1.push_back(abc);
+		}
+		vector<int>mini_inde;
+		int mini = input.size() + 1;
+		//compute minimum number of 1's 
+		for (int i = 0; i < count_1.size(); i++) {
+			if (mini > count_1[i] && count_1[i] > 0) { mini = count_1[i]; }
+		}
+		//find columns where number of ones are min
+		for (int i = 0; i < count_1.size(); i++) {
+			if (count_1[i] == mini) { mini_inde.push_back(i); }
+		}
+		//from column note the index of row where there is 1
+		vector<int>notej;
+		for (int i = 0; i < mini_inde.size(); i++) {
+			for (int j = 0; j < input.size(); j++) {
+				if (gra[j][mini_inde[i]] == 1) { notej.push_back(j); }
+			}
+		}
+
+		//remove repeated elements
+		sort(notej.begin(), notej.end());
+		vector<int>::iterator it;
+		it = unique(notej.begin(), notej.end());
+		notej.resize(std::distance(notej.begin(), it));
+		//note number of ones in selected rows
+		vector<pair<int, int> >row_1;
+		for (int i = 0; i < notej.size(); i++) {
+			int rubber = 0;
+			for (int j = 0; j < n; j++) {
+				if (gra[notej[i]][j] == 1) { rubber++; }
+			}
+			row_1.push_back(make_pair(rubber, notej[i]));
+		}
+		//find max of number of the 1s in row
+		int max = 0;
+		int notein = 0;
+		for (int i = 0; i < row_1.size(); i++) {
+			if (max < row_1[i].first) { max = row_1[i].first; notein = row_1[i].second; }
+		}
+
+		output.push_back(input[notein]);
+		for (int i = 0; i < n; i++) {
+			if (gra[notein][i] == 1) {
+				for (int j = 0; j < input.size(); j++) {
+					gra[j][i] = 0;
+				}
+			}
+		}
+		count_1.clear();
+		mini_inde.clear();
+		notej.clear();
+		row_1.clear();
+	}
+	sort(output.begin(), output.end());
+	auto it = unique(output.begin(), output.end());
+	output.resize(std::distance(output.begin(), it));
+
+	for (auto i = 0; i < output.size(); i++) {
+		unsigned short mask = 0xffff;
+		for (auto j = 0; j < output[i].size(); j++) {
+			if (output[i][j] == '-')
+			{
+				unsigned short newMask = 1 << (15 - j);
+				mask = mask ^ newMask;
+				output[i][j] = '0';
+			}
+		}
+
+		//cout << "0x" << hex << mask << endl;
+		//cout << "0x" << hex << stoi(output[i], nullptr, 2) << endl;
+
+
+		st_port_mask* port_mask = new st_port_mask;
+		port_mask->mask = mask;
+		port_mask->port = stoi(output[i], nullptr, 2);
+		port_mask_vector.push_back(port_mask);
+	}
+
+	return 0;
 }
