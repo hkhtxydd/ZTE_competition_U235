f474e0e10ade665a3d37614b1024041a7a2e45c9
hkhtxydd <13096353290@163.com>
2019-04-09 15:45:39 +0800

    add annotations

--- ZTE_competition/main.cpp
+++ ZTE_competition/main.cpp
@@ -1,6 +1,21 @@
-﻿#define MEM_CHECK false
-#define USING_FILE true
-#define MEM_CLEAR false
+﻿/*
+MEM_CHECK 是否检测内存泄漏，仅用于vs
+USING_FILE 是否使用文件替代标准输入与标准输出
+MEM_CLEAR 是否在程序结束时清理内存（规则）
+*/
+
+/*
+个人认为题目的核心在于逻辑函数化简。假如端口为10：50，那么需要对逻辑函数  F=m10+m11+m12+...+m49+m50  进行化简。
+通过卡诺图与QM算法可以求得逻辑函数的最简形式，但是np的。查找资料得知，实际应用中一般使用Espresso heuristic logic minimizer
+对逻辑函数化简。
+本程序基于“连续”的特性，对其进行化简。并与Espresso heuristic logic minimizer求得的结果进行比较。实验得知，
+本程序的方法非最优解，但与最优解接近。
+
+*/
+
+#define MEM_CHECK false 
+#define USING_FILE false  
+#define MEM_CLEAR false   
 
 #if MEM_CHECK
 #include <crtdbg.h> 
@@ -32,21 +47,34 @@
 
 using namespace std;
 
+//定义无符号整型
 typedef unsigned int UINT32;
 typedef unsigned short UINT16;
 typedef unsigned char UINT8;
 
+//定义无符号整型的最大值
 #define UINT32_MAX ((UINT32)0xffffffff)
 #define UINT16_MAX ((UINT16)0xffff)
 #define UINT8_MAX  ((UINT8)0xff)
 
+//将前缀匹配位数的形式转化为掩码，减小内存占用
 #define prefix2Mask(prefix)  (UINT32_MAX << (32 - (prefix)))
 
+/*
+使用D_port_num与S_port_num控制data/mask的格式，当D_port_num=i，S_port_num=j时，data/mask格式如下：
+目的IP | 源IP | 目的端口_1 | 目的端口_2 | ...... | 目的端口_i | 源端口_1 | 源端口_2 | ...... | 源端口_j | 协议
+(假如i=4，目的端口为1：3，那么目的端口_1为1，目的端口_2为2，目的端口_3为3，目的端口_4也为3以进行填充。)
+其中目的端口_1至目的端口_i被合并输出，源端口_1至源端口_j被合并输出，即输出的第一行为：
+32 32 16*i 16*j 8 。
+在匹配时，将报文转换为：
+目的IP | 源IP | 目的端口 | 目的端口 | ...... | 目的端口 | 源端口 | 源端口 | ...... | 源端口 | 协议
+假如报文的目的端口为2，那么就可以与规则的目的端口_2匹配，则认为报文与规则的目的端口匹配了。源端口的匹配方法相同。
+*/
 #define D_port_num  4
-#define S_port_num  4
+#define S_port_num  5
 
-#define T_match(A,B,C)   ((((A)^(B))&C)==0)
-// T内存匹配模式·
+#define T_match(A,B,C)   ((((A)^(B))&(C))==0)
+// T内存匹配模式
 // ((Input xor data) and mask)
 
 #if USING_FILE
@@ -54,6 +82,7 @@ ifstream inFile;
 ofstream outFile;
 #endif
 
+//data_mask对
 struct st_data_mask
 {
 	UINT16 data;
@@ -66,6 +95,18 @@ struct st_data_mask
 	}
 };
 
+//报文
+struct st_message
+{
+	UINT32 destination_ip;
+	UINT32 source_ip;
+	UINT16 destination_port;
+	UINT16 source_port;
+	UINT8 protocol;
+};
+
+
+//存储规则的结构体
 struct st_rule
 {
 	UINT32 destination_ip;
@@ -84,6 +125,7 @@ struct st_rule
 
 };
 
+//找到最右侧的1
 UINT8 findOne(UINT16 num) {
 	for (UINT8 i = 0; i < 16; i++)
 	{
@@ -144,7 +186,7 @@ void Simplify(vector<st_data_mask*> & result, UINT16 m_begin, UINT16 m_end) {
 
 
 
-
+//工具，字符串分割
 void SplitString(const string& s, vector<string>& v, const string& c) {
 	v.clear();
 	string::size_type pos1, pos2;
@@ -163,7 +205,7 @@ void SplitString(const string& s, vector<string>& v, const string& c) {
 
 
 
-
+//字符串转ip
 void str2ip(const string& s, UINT32 & ip) {
 	vector<string> tmp_1;
 	SplitString(s, tmp_1, ".");
@@ -171,6 +213,7 @@ void str2ip(const string& s, UINT32 & ip) {
 		(((UINT32)(stoi(tmp_1[2]))) << 8) + (UINT32)(stoi(tmp_1[3]));
 }
 
+//字符串转ip与掩码
 void str2int_IpAndMask(const string& s, UINT32 & ip, UINT8 & prefixLength) {
 	vector<string> tmp_1;
 	SplitString(s, tmp_1, "/");
@@ -178,6 +221,7 @@ void str2int_IpAndMask(const string& s, UINT32 & ip, UINT8 & prefixLength) {
 	prefixLength = stoi(tmp_1[1]);
 }
 
+//字符串转协议
 void str2int_protocol(const string& s, UINT8 & protocol) {
 	vector<string> tmp_1;
 	SplitString(s, tmp_1, "/");
@@ -187,6 +231,7 @@ void str2int_protocol(const string& s, UINT8 & protocol) {
 	protocol = (UINT8)stoi(tmp_2[1], nullptr, 16);
 }
 
+//格式化输出规则
 void rule2dataAndMask(st_rule * oneRule) {
 	int i = 0;
 	myStreamOut << "data:";
@@ -212,6 +257,7 @@ void rule2dataAndMask(st_rule * oneRule) {
 	myStreamOut << "0x" << hex << (UINT32_MAX << (32 - (oneRule->destination_ip_prefixLength))) << " ";
 	myStreamOut << "0x" << hex << (UINT32_MAX << (32 - (oneRule->source_ip_prefixLength))) << " ";
 
+	//合并输出端口
 	myStreamOut << "0x";
 	for (i = 0; i < D_port_num; i++) {
 		myStreamOut << hex << oneRule->destination_port[i]->mask;
@@ -227,16 +273,9 @@ void rule2dataAndMask(st_rule * oneRule) {
 	myStreamOut << "0xff" << endl;
 }
 
-struct st_message
-{
-	UINT32 destination_ip;
-	UINT32 source_ip;
-	UINT16 destination_port;
-	UINT16 source_port;
-	UINT8 protocol;
-};
 
 
+//字符串转规则
 void str2rules(vector<st_rule * > & rules, const string rule_str) {
 	UINT32 i, j, k, l;
 	UINT32 destination_ip;
@@ -271,6 +310,7 @@ void str2rules(vector<st_rule * > & rules, const string rule_str) {
 	vector<st_data_mask*>  destination_port_mask_vector;
 	vector<st_data_mask*>  source_port_mask_vector;
 
+	//若开始端口等于结束端口，则直接将其假如用于匹配的端口
 	if (destination_port_begin == destination_port_end) {
 		st_data_mask* port_mask = New st_data_mask;
 		port_mask->mask = UINT16_MAX;
@@ -278,6 +318,7 @@ void str2rules(vector<st_rule * > & rules, const string rule_str) {
 		destination_port_mask_vector.push_back(port_mask);
 	}
 	else {
+		//调用逻辑函数简化函数
 		Simplify(destination_port_mask_vector, destination_port_begin, destination_port_end);
 	}
 
@@ -325,6 +366,7 @@ void str2rules(vector<st_rule * > & rules, const string rule_str) {
 		}
 	}
 
+	//清理内存
 	for (auto & x : destination_port_mask_vector) {
 		delete x;
 		x = nullptr;
@@ -338,7 +380,7 @@ void str2rules(vector<st_rule * > & rules, const string rule_str) {
 	source_port_mask_vector.clear();
 }
 
-
+//比较报文与规则
 void compareMessageAndRules(const ostream & output, const vector<st_rule * > rules, const string message_str) {
 	UINT32 i, j, k;
 	vector<string> oneMessage_split;
@@ -364,6 +406,8 @@ void compareMessageAndRules(const ostream & output, const vector<st_rule * > rul
 
 		bool result_destination_port = false;
 		bool result_source_port = false;
+
+		//多个端口中，只要有一个匹配，即可认为匹配成功
 		for (k = 0; k < D_port_num; k++) {
 			result_destination_port = result_destination_port || T_match(oneMessage_destination_port, oneRule->destination_port[k]->data, oneRule->destination_port[k]->mask);
 		}
@@ -386,7 +430,10 @@ void compareMessageAndRules(const ostream & output, const vector<st_rule * > rul
 
 int main()
 {
+#if MEM_CHECK
 	//_CrtSetBreakAlloc(172);
+#endif
+	//使用程序块，减少内存泄漏检测的误报
 	{
 		UINT32 i = 0;
 
@@ -408,11 +455,10 @@ int main()
 		}
 
 		//输出规则
-		myStreamOut << "32 32 64 64 8 " << rules.size() << endl;
+		myStreamOut << "32 32 64 80 8 " << rules.size() << endl;
 		for (i = 0; i < rules.size(); i++) {
 			rule2dataAndMask(rules[i]);
 		}
-
 
 		vector<string> oneMessage_split;
 
